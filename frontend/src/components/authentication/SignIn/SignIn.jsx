5cbb83c41641e3350c9e6a5cedfed0a25e0366b1
ranjithkumark8 <ranjithkumark8@gmail.com>
2021-05-31 23:26:31 +0530

    few minor changes

--- frontend/src/components/authentication/SignIn/SignIn.jsx
+++ frontend/src/components/authentication/SignIn/SignIn.jsx
@@ -5,10 +5,19 @@ import * as Yup from "yup";
 import "./SignIn.css"
 import { useDispatch, useSelector } from 'react-redux';
 import { signInDetails } from '../../../Redux/AuthRedux/action';
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+
+const override = css`
+  display: block;
+  margin: 50px auto;`;
+
 export const SignIn = () => {
     const dispatch = useDispatch()
     const history = useHistory()
     const token = useSelector((state) => state.authReducer.token)
+    const isLoading = useSelector((state) => state.authReducer.isLoading)
+    // console.log(isLoading)
     if (token) {
         history.push("/")
     }
@@ -47,60 +56,62 @@ export const SignIn = () => {
 
     return (
         <>
-            <div className="formWrapper">
-                <form className="baseForm" onSubmit={handleSubmit} noValidate>
-                    {/* Form Header */}
-                    <header className="baseFormHeader">
-                        <h1 className="baseFormHeading">Customer Login</h1>
-                    </header>
-                    {/* Form main content */}
-                    <div className="formRow col-1">
+            {isLoading ? <ClipLoader color="#B73535" loading={isLoading} css={override} size={150} /> : (
+                <div className="formWrapper">
+                    <form className="baseForm" onSubmit={handleSubmit} noValidate>
+                        {/* Form Header */}
+                        <header className="baseFormHeader">
+                            <h1 className="baseFormHeading">Customer Login</h1>
+                        </header>
+                        {/* Form main content */}
+                        <div className="formRow col-1">
+                            <div className="formFieldWrap">
+                                <label className="formFieldLabel" htmlFor="email">
+                                    Email address
+                                </label>
+
+                                <div className="formFieldWrapInner">
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        className="email formField"
+                                        {...getFieldProps("email")}
+                                    />
+
+                                    <span className="errorMessage">
+                                        {touched["email"] && errors["email"]}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                         <div className="formFieldWrap">
-                            <label className="formFieldLabel" htmlFor="email">
-                                Email address
+                            <label className="formFieldLabel" htmlFor="password">
+                                Password
                             </label>
 
                             <div className="formFieldWrapInner">
                                 <input
-                                    type="email"
-                                    id="email"
-                                    className="email formField"
-                                    {...getFieldProps("email")}
+                                    type="password"
+                                    id="password"
+                                    className="password formField"
+                                    {...getFieldProps("password")}
                                 />
-
                                 <span className="errorMessage">
-                                    {touched["email"] && errors["email"]}
+                                    {touched["password"] && errors["password"]}
                                 </span>
+                                <span>Forgot your password?</span>
                             </div>
+                            {/* <span className="passwordCue">Minimum length is 8 characters</span> */}
                         </div>
-                    </div>
-                    <div className="formFieldWrap">
-                        <label className="formFieldLabel" htmlFor="password">
-                            Password
-                        </label>
-
-                        <div className="formFieldWrapInner">
-                            <input
-                                type="password"
-                                id="password"
-                                className="password formField"
-                                {...getFieldProps("password")}
-                            />
-                            <span className="errorMessage">
-                                {touched["password"] && errors["password"]}
-                            </span>
-                            <span>Forgot your password?</span>
+                        <div className="submitBtnContainer">
+                            <button type="submit">Sign In</button>
                         </div>
-                        {/* <span className="passwordCue">Minimum length is 8 characters</span> */}
+                    </form>
+                    <div className="register_header">
+                        <Link to="/Register">No Account ? Register Here</Link>
                     </div>
-                    <div className="submitBtnContainer">
-                        <button type="submit">Sign In</button>
-                    </div>
-                </form>
-                <div className="register_header">
-                    <Link to="/Register">No Account ? Register Here</Link>
                 </div>
-            </div>
+            )}
         </>
     );
 }
